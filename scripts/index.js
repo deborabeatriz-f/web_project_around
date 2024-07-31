5bffa2f3d2a15d6bf30c3fc4bdda672a4f92ecc9
deborabeatrizf <deborabeatriz.f@gmail.com>
2024-07-31 10:15:53 -0300

    update javascript file

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,35 @@
 // PROFILE EDIT BUTTON
-
-let modal = document.querySelector(".profile__modal");
+let modal = document.querySelector(".modal");
 let editButton = document.querySelector(".profile__button-edit");
 let addButton = document.querySelector(".input__submit-add");
 let closeEditButton = document.querySelector(".modal__button-close");
-let page = document.querySelector(".page");
+let page = document.querySelector(".page__overlay");
 
 function appearEditPopUp() {
   modal.style.display = "block";
-  // FILTER - ADICIONAR CAMADA - COR
-  // page.style.filter = "blur (10px)";
+  page.style.display = "block"
 }
 editButton.addEventListener("click", appearEditPopUp);
 
 function closeEditPopUp() {
   modal.style.display = "none";
-  // FILTER - RETIRAR CAMADA - COR
-  // page.style.filter = "blur (0px)";
+   page.style.display = "none";
 }
 closeEditButton.addEventListener("click", closeEditPopUp);
-modal.addEventListener("click", closeEditPopUp);
 
 //INSERT HTML PROFILE INFOS FROM EDIT
+function addProfileInfo() {
+  let name = document.querySelector(".profile__title");
+  let job = document.querySelector(".profile__subtitle");
+  let addName = document.querySelector(".input__text-name");
+  let addJob = document.querySelector(".input__text-job");
+
+  name.textContent = addName.value;
+  job.textContent = addJob.value;
+}
+addButton.addEventListener("click", addProfileInfo);
 
 // HEART(LIKE) BUTTON
-
 let hearts = document.querySelectorAll("#grid__button-heart");
 
 function heartButton(event) {
@@ -37,5 +42,4 @@ function heartButton(event) {
     heart.classList.add("grid__button-heart");
   }
 }
-
 hearts.forEach((heart) => heart.addEventListener("click", heartButton));
