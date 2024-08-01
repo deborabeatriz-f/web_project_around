b6b281beda5066393634408c20c0a9a15b41d2c4
deborabeatrizf <deborabeatriz.f@gmail.com>
2024-07-31 21:03:03 -0300

    update js file

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,7 @@ let editButton = document.querySelector(".profile__button-edit");
 let addButton = document.querySelector(".input__submit-add");
 let closeEditButton = document.querySelector(".popup__button-close");
 let page = document.querySelector(".popup");
+let popupEditProfile = document.querySelector(".popup__input");
 
 // PROFILE EDIT POPUP OPEN
 function appearEditPopUp() {
@@ -20,7 +21,9 @@ function closeEditPopUp() {
 closeEditButton.addEventListener("click", closeEditPopUp);
 
 // GET PROFILE INFOS FROM INPUT
-function addProfileInfo() {
+function addProfileInfo(event) {
+  event.preventDefault();
+
   let name = document.querySelector(".profile__title");
   let job = document.querySelector(".profile__subtitle");
   let addName = document.querySelector(".input__text-name");
@@ -28,8 +31,10 @@ function addProfileInfo() {
 
   name.textContent = addName.value;
   job.textContent = addJob.value;
+
+  closeEditPopUp();
 }
-addButton.addEventListener("click", addProfileInfo);
+popupEditProfile.addEventListener("submit", addProfileInfo);
 
 // HEART(LIKE) BUTTON
 let hearts = document.querySelectorAll("#grid__button-heart");
